d22e4a3c09239f860a7bfbee92c877dc5995afdb
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-11 12:45:11 -0700

    Initial Template

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, "/src/index.ts"),
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "/public/index.html"),
+            inject: "body"
+        })
+    ],
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './src/public',  //source of static assets
+        port: 7700, // port to run dev-server
+    } 
+}
